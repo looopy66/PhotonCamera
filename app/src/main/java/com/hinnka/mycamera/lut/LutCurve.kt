566d1826f1d4373bab112714b5ae675b749f67fe
Hinnka <bjzhou1990@gmail.com>
2026-02-01 12:09:47 +0800

    fix: curve

--- app/src/main/java/com/hinnka/mycamera/lut/LutCurve.kt
+++ app/src/main/java/com/hinnka/mycamera/lut/LutCurve.kt
@@ -22,11 +22,11 @@ enum class LutCurve {
             LINEAR -> linear
             V_LOG -> {
                 if (linear < 0.01f) 5.6f * linear + 0.125f
-                else 0.421248f * log10(5.555556f * linear + 0.047996f) + 0.383577f
+                else 0.241514f * log10(linear + 0.00873f) + 0.598206f
             }
             S_LOG3 -> {
-                if (linear < 0.01125f) (171.2102946929f * linear + 95f) / 1023f
-                else (422f * log10((linear + 0.01f) / (0.18f + 0.01f)) + 616f) / 1023f
+                if (linear < 0.01125f) (linear * (171.2102946929f - 95f) / 0.01125f + 95f) / 1023f
+                else (420f + log10((linear + 0.01f) / (0.18f + 0.01f)) * 261.5f) / 1023f
             }
             F_LOG2 -> {
                 // F-Log2 formula from Fujifilm
@@ -34,13 +34,20 @@ enum class LutCurve {
                 else 0.245281f * log10(5.555556f * linear + 0.064829f) + 0.384316f
             }
             LOG_C -> {
-                if (linear > 0.010591f) 0.247190f * log10(1.702f * linear + 0.043519f) + 0.385537f
-                else 5.613235f * linear + 0.092809f
+                if (linear > 0.010591f) 0.247190f * log10(5.555556f * linear + 0.052272f) + 0.385537f
+                else 5.367655f * linear + 0.092809f
             }
             APPLE_LOG -> {
-                // Apple Log formula from Apple Log Profile White Paper
-                if (linear > 0.019242594858951f) 0.081519922730206f * ln(47.3665233297121f * linear + 0.0028231596425514f) + 0.453351173665842f
-                else 9.50854453301014f * linear + 0.063718911571732f
+                // Apple Log formula from Apple Log Profile White Paper (September 2023)
+                // R0 = -0.05641088, Rt = 0.01
+                // c = 47.28711236, beta = 0.00964052, gamma = 0.08550479, delta = 0.69336945
+                if (linear >= 0.01f) {
+                    0.08550479f * log2(linear + 0.00964052f) + 0.69336945f
+                } else if (linear >= -0.05641088f) {
+                    47.28711236f * (linear + 0.05641088f).pow(2f)
+                } else {
+                    0f
+                }
             }
             HLG -> {
                 val a = 0.17883277f
@@ -62,23 +69,30 @@ enum class LutCurve {
             LINEAR -> value
             V_LOG -> {
                 if (value < 0.181f) (value - 0.125f) / 5.6f
-                else (10f.pow((value - 0.383577f) / 0.421248f) - 0.047996f) / 5.555556f
+                else 10f.pow((value - 0.598206f) / 0.241514f) - 0.00873f
             }
             S_LOG3 -> {
-                if (value < 171.2102946929f * 0.01125f / 1023f) (value * 1023f - 95f) / 171.2102946929f
-                else 10f.pow((value * 1023f - 616f) / 422f) * (0.18f + 0.01f) - 0.01f
+                if (value < 171.2102946929f / 1023f) (value * 1023f - 95f) * 0.01125f / (171.2102946929f - 95f)
+                else 10f.pow((value * 1023f - 420f) / 261.5f) * (0.18f + 0.01f) - 0.01f
             }
             F_LOG2 -> {
                 if (value < 0.100686685370811f) (value - 0.092864f) / 8.799461f
                 else (10f.pow((value - 0.384316f) / 0.245281f) - 0.064829f) / 5.555556f
             }
             LOG_C -> {
-                if (value > 5.613235f * 0.010591f + 0.092809f) (10f.pow((value - 0.385537f) / 0.247190f) - 0.043519f) / 1.702f
-                else (value - 0.092809f) / 5.613235f
+                if (value > 0.149658f) (10f.pow((value - 0.385537f) / 0.247190f) - 0.052272f) / 5.555556f
+                else (value - 0.092809f) / 5.367655f
             }
             APPLE_LOG -> {
-                if (value > 0.246602353f) (exp((value - 0.453351173665842f) / 0.081519922730206f) - 0.0028231596425514f) / 47.3665233297121f
-                else (value - 0.063718911571732f) / 9.50854453301014f
+                // Pt = c * (Rt - R0)^2 = 47.28711236 * (0.01 + 0.05641088)^2 ≈ 0.2088
+                val pt = 0.20883119f
+                if (value >= 0.20883119f) {
+                    2.0f.pow((value - 0.69336945f) / 0.08550479f) - 0.00964052f
+                } else if (value > 0f) {
+                    sqrt(value / 47.28711236f) - 0.05641088f
+                } else {
+                    -0.05641088f
+                }
             }
             HLG -> {
                 val a = 0.17883277f
